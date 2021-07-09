0217ba6b28e3c774b9f25907996c2db3ccbe952d
Alex N <alex@alexandarnarayan.com>
2021-07-09 23:02:25 +0200

    ...

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -4,7 +4,6 @@ from hashlib import sha3_512
 
 def test_hashfunction_0():
     d = 'hello world' 
-    print(sha3_512(d.encode()))
     assert sha3_512(d.encode()).hexdigest() == '840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a'
 
 def test_hashfunction_1():
