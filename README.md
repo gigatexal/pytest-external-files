35e966861a3274f41c132e19641ddd3a43f3bb85
Alex N <alex@alexandarnarayan.com>
2021-07-09 22:55:56 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# It's not entirely clear how to best reference external files in tests
+
+### recreate by following these steps
+
+1. python3 -m venv .venv
+2. source .venv/bin/activate
+3. pip3 install -r requirements.txt
+4. pytest
+
+### expectations:
+
+Test 1 should pass it doesn't use an external file
+Test 2 should fail because it depends on an external file
---
+++ tests/test_hashing.py
@@ -0,0 +1,15 @@
+import pytest
+
+from hashlib import sha3_512
+
+def test_hashfunction_0():
+    d = 'hello world' 
+    print(sha3_512(d.encode()))
+    assert sha3_512(d.encode()).hexdigest() == '840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a'
+
+def test_hashfunction_1():
+    with open('./data/dummy.txt') as data:
+        d = data.read() # text should be 'hello world'
+        print(sha3_512(d.encode()))
+        assert sha3_512(d.encode()) == 'foo'
+
