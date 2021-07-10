79e7cc2ce697f741ec307a0130631c15d04c9ca5
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-10 09:18:31 +0200

    deduplicate using constant

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -2,12 +2,15 @@ import pytest
 
 from hashlib import sha3_512
 
+EXPECTED_HASH = '840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a'
+
+
 def test_hashfunction_0():
     d = 'hello world'
-    assert sha3_512(d.encode()).hexdigest() == '840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a'
+    assert sha3_512(d.encode()).hexdigest() == EXPECTED_HASH
 
 def test_hashfunction_1():
     with open('tests/data/dummy.txt') as data:
-        d = data.read().strip() # text should be 'hello world'
-        assert sha3_512(d.encode()).hexdigest() == '840006653e9ac9e95117a15c915caab81662918e925de9e004f774ff82d7079a40d4d27b1b372657c61d46d470304c88c788b3a4527ad074d1dccbee5dbaa99a'
+        d = data.read().strip()
+        assert sha3_512(d.encode()).hexdigest() == EXPECTED_HASH
 
